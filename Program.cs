2d58f00f6dc527fcb06eec4f13bf7c42ce224a37
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-07-12 09:47:55 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide username and password:");
+
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                Console.WriteLine("Please provide username and password.");
+            }
+            else if (username == "admin1256" && password == "admin@1256@1256")
+            {
+                Console.WriteLine("Sign Success");
+            }
+            else
+            {
+                Console.WriteLine("Invalid user.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
